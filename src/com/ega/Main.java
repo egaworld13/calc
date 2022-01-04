ebb97bafeddedf3aebd27dc67e2217ab309169b7
Edgars <egaworld13@users.noreply.github.com>
2022-01-04 21:48:22 +0200

    calc

---
+++ src/com/ega/Main.java
@@ -0,0 +1,31 @@
+package com.ega;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1;
+        int num2;
+        int oper;
+
+        //1. skaitlis
+        System.out.println("Ievadiet 1. skaitli:");
+
+        System.out.println("");
+        num1 = scanner.nextInt();
+        //darbība
+        System.out.println("Ievadiet darbību");
+
+        System.out.println("");
+        oper = scanner.nextInt();
+
+
+        //2. skaitlis
+        System.out.println("Ievadiet 2. skaitli:");
+
+        System.out.println("");
+        num2 = scanner.nextInt();
+
+        }
+    }
+
