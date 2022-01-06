a3f9889dfb22068b8e8ce30e3e838a056a0f857f
Edgars <egaworld13@users.noreply.github.com>
2022-01-06 17:06:41 +0200

    Final

--- src/com/ega/Main.java
+++ src/com/ega/Main.java
@@ -1,37 +1,44 @@
 package com.ega;
+import static java.lang.System.out;
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        //1. skaitlis
-        System.out.println("Ievadiet 1. skaitli:");
-
-        System.out.println("");
-        int num1 = scanner.nextInt();
+       //1. skaitlis
+       out.println("1. skaitlis:");
+        while (!sc.hasNextInt()) {
+            out.println("SKAITLI!!!:");
+            sc.next();}
+        int num1 = sc.nextInt();
 
         //darbība
-        System.out.println("Ievadiet '/', '*', '+', '-'  darbību:");
-        System.out.println("");
-        String oper;
-        scanner.nextLine();//nepieciešams,jo nextint nolasa string automātiski, bez ievades iespējas.
-        oper = scanner.nextLine();
-
-
-        //2. skaitlis
-        System.out.println("Ievadiet 2. skaitli:");
-
-        System.out.println("");
-        int num2 = scanner.nextInt();
-
-
-
-        System.out.println("Rezultāts:");
-        if(oper.equals("+")) System.out.println(num1 + num2);
-        if(oper.equals("-")) System.out.println(num1 - num2);
-        if(oper.equals("*")) System.out.println(num1 * num2);
-        if(oper.equals("/")) System.out.println(num1 / num2);
+        boolean darbiba = true;
+        String oper = null; // oper piešķīru 0 vērtību, jo switch met kļūdu. Kodā oper vērtība tiks piešķirta pēc ievades.
+        while (darbiba) {
+            out.println("Ievadiet '/', '*', '+', '-'  darbību:");
+            sc.nextLine();//nepieciešams,jo nextint nolasa string automātiski, bez ievades iespējas.
+            oper = sc.nextLine();
+            if (oper.equals("+") || oper.equals("-") || oper.equals("/") || oper.equals("*")) {
+                darbiba = false;
+            }
         }
+        //2. skaitlis
+        out.println("2. skaitlis:");
+        while (!sc.hasNextInt()) {
+            out.println("Veselu skaitli!!!:");
+            sc.next();
+        }
+        int num2 = sc.nextInt();
+
+        out.println("Rezultāts:");
+        switch (oper) {
+            case "+" -> out.println(num1 + num2);
+            case "-" -> out.println(num1 - num2);
+            case "*" -> out.println(num1 * num2);
+            case "/" -> out.println(num1 / num2);
+        }
+    }
     }
 
