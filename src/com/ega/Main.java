a115ead8100c93bb90189459938f8d551f0c0b03
Edgars <egaworld13@users.noreply.github.com>
2022-01-09 14:03:32 +0200

    Atkārtojas

--- src/com/ega/Main.java
+++ src/com/ega/Main.java
@@ -6,39 +6,48 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       //1. skaitlis
-       out.println("1. skaitlis:");
-        while (!sc.hasNextDouble()) {
-            out.println("SKAITLI!!!:");
-            sc.next();}
-        double num1 = sc.nextDouble();
-
-        //darbība
-        boolean darbiba = true;
-        String oper = null; // oper piešķīru 0 vērtību, jo switch met kļūdu. Kodā oper vērtība tiks piešķirta pēc ievades.
-        while (darbiba) {
-            out.println("Ievadiet '/', '*', '+', '-'  darbību:");
-            sc.nextLine();//nepieciešams,jo nextint nolasa string automātiski, bez ievades iespējas.
-            oper = sc.nextLine();
-            if (oper.equals("+") || oper.equals("-") || oper.equals("/") || oper.equals("*")) {
-                darbiba = false;
+        String izv;
+        do {
+            //1. skaitlis
+            out.println("1. skaitlis:");
+            while (!sc.hasNextDouble()) {
+                out.println("SKAITLI!!!:");
+                sc.next();
             }
-        }
-        //2. skaitlis
-        out.println("2. skaitlis:");
-        while (!sc.hasNextDouble()) {
-            out.println("Veselu skaitli!!!:");
-            sc.next();
-        }
-        double num2 = sc.nextDouble();
+            double num1 = sc.nextDouble();
 
-        out.println("Rezultāts:");
-        switch (oper) {
-            case "+" -> out.println(num1 + num2);
-            case "-" -> out.println(num1 - num2);
-            case "*" -> out.println(num1 * num2);
-            case "/" -> out.println(num1 / num2);
+            //darbība
+            boolean darbiba = true;
+            String oper = null; // oper piešķīru 0 vērtību, jo switch met kļūdu. Kodā oper vērtība tiks piešķirta pēc ievades.
+            while (darbiba) {
+                out.println("Ievadiet '/', '*', '+', '-'  darbību:");
+                sc.nextLine();//nepieciešams,jo nextint nolasa string automātiski, bez ievades iespējas.
+                oper = sc.nextLine();
+                if (oper.equals("+") || oper.equals("-") || oper.equals("/") || oper.equals("*")) {
+                    darbiba = false;
+                }
+            }
+            //2. skaitlis
+            out.println("2. skaitlis:");
+            while (!sc.hasNextDouble()) {
+                out.println("Veselu skaitli!!!:");
+                sc.next();
+            }
+            double num2 = sc.nextDouble();
+
+            out.println("Rezultāts:");
+            switch (oper) {
+                case "+" -> out.println(num1 + num2);
+                case "-" -> out.println(num1 - num2);
+                case "*" -> out.println(num1 * num2);
+                case "/" -> out.println(num1 / num2);
+            }
+            out.println("Vēlaties veikt nākošo aprēķinu? y/n?");
+            sc.nextLine();
+            izv = sc.nextLine();
         }
+        while (izv.equals("y"));
+
     }
     }
 
